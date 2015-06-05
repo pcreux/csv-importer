c619053ea8b8a2fd78bc87fb507397231497f6fe
Philippe Creux <pcreux@gmail.com>
2015-06-05 12:21:24 -0700

    Document methods and classes

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -3,6 +3,8 @@ require "csv"
 require "virtus"
 
 module CSVImporter
+
+  # Reads, sanitize and parse a CSV file
   class CSVReader
     include Virtus.model
 
@@ -14,10 +16,12 @@ module CSVImporter
       @csv_rows ||= sanitize_cells(CSV.parse(sanitize_content(read_content)))
     end
 
+    # Returns the header as an Array of Strings
     def header
       @header ||= csv_rows.first
     end
 
+    # Returns the rows as an Array of Arrays of Strings
     def rows
       @rows ||= csv_rows[1..-1]
     end
@@ -36,6 +40,7 @@ module CSVImporter
       end
     end
 
+    # Strip cells
     def sanitize_cells(rows)
       rows.map do |cells|
         cells.map do |cell|
@@ -44,23 +49,54 @@ module CSVImporter
       end
     end
 
+    # Replaces windows line separators with "\n"
     def sanitize_content(csv_content)
-      csv_content.gsub(/\r\r?\n?/, "\n") # handle windows line separators
+      csv_content.gsub(/\r\r?\n?/, "\n")
     end
   end
 
+  # Define a column. Called from the DSL via `column.
+  #
+  # Examples
+  #
+  #   # the csv column "email" will be assigned to the `email` attribute
+  #   column :email
+  #
+  #   # the csv column matching /email/i will be assigned to the `email` attribute
+  #   column :email, as: /email/i
+  #
+  #   # the csv column matching "First name" or "Prénom" will be assigned to the `first_name` attribute
+  #   column :first_name, as: [/first ?name/i, /pr(é|e)nom/i]
+  #
+  #   # the csv column "first_name" will be assigned to the `f_name` attribute
+  #   column :first_name, to: :f_name
+  #
+  #   # email will be downcased
+  #   column :email, to: ->(email) { email.downcase }
+  #
+  #   # transform `confirmed` to `confirmed_at`
+  #   column :confirmed, to: ->(confirmed, model) do
+  #     model.confirmed_at = confirmed == "true" ? Time.new(2012) : nil
+  #   end
+  #
   class ColumnDefinition
     include Virtus.model
 
     attribute :name, Symbol
-    attribute :to
-    attribute :as
+    attribute :to # Symbol or Proc
+    attribute :as # Symbol, String, Regexp, Array
     attribute :required, Boolean
 
+    # The model attribute that this column targets
     def attribute
-      to || name
+      if to.is_a?(Symbol)
+        to
+      else
+        name
+      end
     end
 
+    # Return true if column definition matches the column name passed in.
     def match?(column_name, search_query=(as || name))
       return false if column_name.nil?
 
@@ -82,6 +118,8 @@ module CSVImporter
     end
   end
 
+  # A Column from a CSV file with a `name` (from the csv file) and a matching
+  # `ColumnDefinition` if any.
   class Column
     include Virtus.model
 
@@ -89,6 +127,7 @@ module CSVImporter
     attribute :definition, ColumnDefinition
   end
 
+  # The CSV Header
   class Header
     include Virtus.model
 
@@ -116,7 +155,7 @@ module CSVImporter
 
     # Returns Array[String]
     def required_columns
-      column_definitions.select { |definition| definition.required? }.map(&:name)
+      column_definitions.select(&:required?).map(&:name)
     end
 
     # Returns Array[String]
@@ -147,6 +186,10 @@ module CSVImporter
     end
   end
 
+  # A Row from the CSV file.
+  #
+  # Using the header, the model_klass and the identifier it builds the model
+  # to be persisted.
   class Row
     include Virtus.model
 
@@ -155,6 +198,7 @@ module CSVImporter
     attribute :model_klass
     attribute :identifier
 
+    # The model to be persisted
     def model
       @model ||= begin
         model = if identifier
@@ -168,35 +212,45 @@ module CSVImporter
       end
     end
 
+    # A hash with this row's attributes
     def csv_attributes
       @csv_attributes ||= Hash[header.column_names.zip(row_array)]
     end
 
+    # Set attributes
     def set_attributes(model)
       header.columns.each do |column|
-        csv_value = csv_attributes[column.name]
+        value = csv_attributes[column.name]
         column_definition = column.definition
 
         next if column_definition.nil?
 
-        if column_definition.to && column_definition.to.is_a?(Proc)
-          to_proc = column_definition.to
-
-          case to_proc.arity
-          when 1
-            model.public_send("#{column_definition.name}=", to_proc.call(csv_value))
-          when 2
-            to_proc.call(csv_value, model)
-          else
-            raise "test me error!"
-          end
-        else
-          attribute = column_definition.attribute
-          model.public_send("#{attribute}=", csv_value)
-        end
+        set_attribute(model, column_definition, value)
       end
     end
 
+    # Set the attribute using the column_definition and the csv_value
+    def set_attribute(model, column_definition, csv_value)
+      if column_definition.to && column_definition.to.is_a?(Proc)
+        to_proc = column_definition.to
+
+        case to_proc.arity
+        when 1 # to: ->(email) { email.downcase }
+          model.public_send("#{column_definition.name}=", to_proc.call(csv_value))
+        when 2 # to: ->(published, post) { post.published_at = Time.now if published == "true" }
+          to_proc.call(csv_value, model)
+        else
+          raise ArgumentError, "`to` proc can only have 1 or 2 arguments"
+        end
+      else
+        attribute = column_definition.attribute
+        model.public_send("#{attribute}=", csv_value)
+      end
+
+      model
+    end
+
+    # Error from the model mapped back to the CSV header if we can
     def errors
       Hash[
         model.errors.map do |attribute, errors|
@@ -210,6 +264,14 @@ module CSVImporter
     end
   end
 
+  # The Report you get back from an import.
+  #
+  # * It has a status (pending, invalid_csv_file, invalid_header, in_progress, done, aborted)
+  # * It lists out missing columns
+  # * It reports parser_error
+  # * It lists out (created / updated) * (success / failed) records
+  # * It provides a human readable message
+  #
   class Report
     include Virtus.model
 
@@ -223,6 +285,8 @@ module CSVImporter
     attribute :updated_rows, Array[Row], default: proc { [] }
     attribute :failed_to_create_rows, Array[Row], default: proc { [] }
     attribute :failed_to_update_rows, Array[Row], default: proc { [] }
+
+    attribute :message_generator, Class, default: proc { ReportMessage }
 
     def valid_rows
       created_rows + updated_rows
@@ -255,22 +319,26 @@ module CSVImporter
     def invalid_csv_file!; self.status = :invalid_csv_file; self; end
 
     def message
-      ReportMessage.new(report: self).to_s
+      message_generator.call(self)
     end
   end
 
   class ReportMessage
-    include Virtus.model
+    def self.call(report)
+      new(report).to_s
+    end
 
-    attribute :report, Report
+    def initialize(report)
+      @report = report
+    end
+
+    attr_accessor :report
 
     def to_s
       send("report_#{report.status}")
     end
 
-    def report_invalid_header
-      "The following columns are required: #{report.missing_columns.join(", ")}"
-    end
+    private
 
     def report_pending
       "Import hasn't started yet"
@@ -281,21 +349,35 @@ module CSVImporter
     end
 
     def report_done
-      details = report.attributes
-        .select { |name, _| name["_rows"] }
-        .select { |_, instances| instances.size > 0 }
-        .map { |bucket, instances| "#{instances.size} #{bucket.to_s.gsub('_rows', '').gsub('_', ' ')}" }
-        .join(", ")
+      "Import completed: " + import_details
+    end
 
-      "Import completed: " + details
+    def report_invalid_header
+      "The following columns are required: #{report.missing_columns.join(", ")}"
     end
 
     def report_invalid_csv_file
       report.parser_error
     end
 
+    def report_aborted
+      "Import aborted"
+    end
+
+    # Generate something like: "3 created. 4 updated. 1 failed to create. 2 failed to update."
+    def import_details
+      report.attributes
+        .select { |name, _| name["_rows"] }
+        .select { |_, instances| instances.size > 0 }
+        .map { |bucket, instances| "#{instances.size} #{bucket.to_s.gsub('_rows', '').gsub('_', ' ')}" }
+        .join(", ")
+    end
+
   end
 
+  # Do the actual import.
+  #
+  # It iterates over the rows' models and persist them. It returns a `Report`.
   class Runner
     def self.call(*args)
       new(*args).call
@@ -306,11 +388,12 @@ module CSVImporter
     attribute :rows, Array[Row]
     attribute :when_invalid, Symbol
 
+    attribute :report, Report, default: proc { Report.new }
+
     ImportAborted = Class.new(StandardError)
 
+    # Persist the rows' model and return a `Report`
     def call
-      report = Report.new
-
       if rows.empty?
         report.done!
         return report
@@ -352,6 +435,7 @@ module CSVImporter
     end
   end
 
+  # The configuration of a CSVImporter
   class Config
     include Virtus.model
 
@@ -364,6 +448,8 @@ module CSVImporter
   class Error < StandardError
   end
 
+  # This Dsl extends a class that includes CSVImporter
+  # It is a thin proxy to the Config object
   module Dsl
     def model(model_klass)
       csv_importer_config.model = model_klass
@@ -383,6 +469,20 @@ module CSVImporter
   end
 end
 
+# A class that includes CSVImporter inherit its DSL and methods.
+#
+# Example:
+#   class ImportUserCSV
+#     include CSVImporter
+#
+#     model User
+#
+#     column :email
+#   end
+#
+#   report = ImportUserCSV.new(file: my_csv).run!
+#   puts report.message
+#
 module CSVImporter
   def self.included(klass)
     klass.extend(Dsl)
@@ -391,6 +491,14 @@ module CSVImporter
     end
   end
 
+  # Defines the path, file or content of the csv file.
+  # Also allows you to overwrite the configuration at runtime.
+  #
+  # Example:
+  #
+  #   .new(file: my_csv_file)
+  #   .new(path: "subscribers.csv", model: newsletter.subscribers)
+  #
   def initialize(*args)
     @csv = CSVReader.new(*args)
     @config = self.class.csv_importer_config.dup
@@ -399,15 +507,18 @@ module CSVImporter
 
   attr_reader :csv, :report, :config
 
+  # Initialize and return the `Header` for the current CSV file
   def header
     @header ||= Header.new(column_definitions: config.column_definitions, column_names: csv.header)
   end
 
+  # Initialize and return the `Row`s for the current CSV file
   def rows
     csv.rows.map { |row_array| Row.new(header: header, row_array: row_array,
                                        model_klass: config.model, identifier: config.identifier) }
   end
 
+  # Run the import. Return a Report.
   def run!
     if header.valid?
       @report = Runner.call(rows: rows, when_invalid: config.when_invalid)
