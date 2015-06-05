f12ba630f865e3285365e247ca83c7a4e897e5da
Philippe Creux <pcreux@gmail.com>
2015-06-05 14:19:28 -0700

    Comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 if ENV['CODECLIMATE_REPO_TOKEN']
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
+else
+  require "simplecov"
+  SimpleCov.start
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
