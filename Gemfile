4b82a49163f367318e543eb13aae23f3247f595f
Philippe Creux <pcreux@gmail.com>
2015-06-19 15:56:28 -0700

    Upgrade to latest RSpec to use --only-failures option

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in csv-importer.gemspec
 gemspec
 
-gem 'rspec'
+gem 'rspec', '~> 3.3.0'
 gem 'guard-rspec'
 gem 'activemodel'
 gem 'simplecov', require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_model'
 
 require 'csv_importer'
+
+RSpec.configure do |c|
+  c.example_status_persistence_file_path = "./spec/examples.txt"
+end
