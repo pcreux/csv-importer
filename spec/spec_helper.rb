4699cf0e3f4a671480ad0cd5b7e9a370d76e555a
Philippe Creux <pcreux@gmail.com>
2017-01-06 15:03:25 -0800

    Fix code climate coverage reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-else
-  require "simplecov"
-  SimpleCov.start
-end
+require "simplecov"
+SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
